7e145d60af3e786e20e88f25633e1becb8289683
shubham <shubhamsheelvant@gmail.com>
2026-04-18 14:27:34 +0530

    removed border radius

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,25 @@
 import {sendRequestToGPT} from "./menu.js";
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     let img = document.querySelector("#inputImage");
+//     let container = document.querySelector("#imageInput");
+    
+//     const updateBorderRadius = () => {
+//         if (img.offsetHeight >= 225) {
+//             container.style.borderRadius = "10px";
+//             img.style.borderRadius = "0";
+//         } else {
+//             container.style.borderRadius = "0";
+//             img.style.borderRadius = "0";
+//         }
+//     };
+    
+//     if (img.complete) {
+//         updateBorderRadius();
+//     } else {
+//         img.addEventListener('load', updateBorderRadius);
+//     }
+    
+//     const observer = new ResizeObserver(updateBorderRadius);
+//     observer.observe(img);
+// });
